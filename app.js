865c59b47f161a1b57738281b1c05eb4e4d1ed0f
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2024-08-31 12:32:22 +0300

    Final Project Version 1

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+// const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./server/routes/user');
 
 require('dotenv').config();
 
@@ -11,10 +12,19 @@ const port = process.env.PORT || 5000;
 // Parsing middleware
 app.use(express.urlencoded({extended: true}));
 
+// Path to the views Directory
+// app.set('views', path.join(__dirname, 'server/views'))
+
 // Templating Engine
-app.engine('hbs', engine());
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout:'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
 app.set('view engine', 'hbs');
 
-app.listen(port, () => {
+app.use('/', routes);
+
+app.listen(port, (req, res) => {
     console.log(`App running on Port ${port}`);
 });
